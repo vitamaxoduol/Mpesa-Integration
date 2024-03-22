4f8fb4712b7de6431f0a39584a3b19fcc958225d
vitamaxoduol <vitamaxoduo@gmail.com>
2024-03-22 12:46:43 +0300

    test success

--- Mpesa/apis/auth.py
+++ Mpesa/apis/auth.py
@@ -5,12 +5,14 @@ from requests.auth import HTTPBasicAuth
 
 
 class MpesaBase:
-    def __init__(self, env="sandbox",  CONSUMER_KEY="4lLvX2iuvUpu435BA7uMVBqYWuNBlaRBt3GBP4VMZV97szCY", CONSUMER_SECRET="hUJX9nzaqfAG6AEwvFTqr0OQNQGQTRpFwjzCjkUPvt4YB4cGGObcAupynBu4Mxs5", sandbox_url="https://sandbox.safaricom.co.ke", live_url="https://api.safaricom.co.ke"):
+    def __init__(self, env="sandbox", APP_KEY=None, APP_SECRET=None,  CONSUMER_KEY="4lLvX2iuvUpu435BA7uMVBqYWuNBlaRBt3GBP4VMZV97szCY", CONSUMER_SECRET="hUJX9nzaqfAG6AEwvFTqr0OQNQGQTRpFwjzCjkUPvt4YB4cGGObcAupynBu4Mxs5", sandbox_url="https://sandbox.safaricom.co.ke", live_url="https://api.safaricom.co.ke"):
         self.env = env
         self.CONSUMER_KEY = CONSUMER_KEY
         self.CONSUMER_SECRET = CONSUMER_SECRET
         self.sandbox_url = sandbox_url
         self.live_url = live_url
+        self.APP_KEY = APP_KEY
+        self.APP_SECRET = APP_SECRET
         self.token = None
         self.session = requests.Session()
 
@@ -27,10 +29,11 @@ class MpesaBase:
         try:
             response = self.session.get(authenticate_url, auth=HTTPBasicAuth(self.CONSUMER_KEY, self.CONSUMER_SECRET))
             response.raise_for_status()
-            self.token = response.json().get('acess_token')
+            self.token = response.json().get('access_token')
             return self.token
         except requests.RequestException as e:
-            traceback.print_exec()
+            traceback.print_exc()
             print(f"Error during authentication: {e}")
             return None
+        
 
